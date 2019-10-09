2a85d1b5f4fc2e004ed330c26427e42ab630f1cd
theo-poujol <theo.poujol@etu.univ-amu.fr>
2019-10-09 21:31:42 +0200

    Add comments to RangeArray

--- src/Application/RangeArray.java
+++ src/Application/RangeArray.java
@@ -3,12 +3,24 @@ import javafx.util.Pair;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class RangeArray<T> implements Iterable{
+public class RangeArray<T> implements Iterable {
+
+    /**
+     * Attributs
+     */
 
     private int indexMin;
     private int indexMax;
     private ArrayList<Pair<Integer, T>> array;
 
+    /**
+     * Constructeur
+     *
+     * @param indexMin indice Minimum
+     * @param indexMax indice Maximum
+     *
+     * @throws IndiceInferieurException Exception si l'indice minimum est plus grand que l'indice maximum
+     */
 
     public RangeArray(int indexMin, int indexMax) throws IndiceInferieurException {
 
@@ -24,6 +36,13 @@ public class RangeArray<T> implements Iterable{
 
     }
 
+    /**
+     * Fonction get générique
+     *
+     * @param index index du tableau à renvoyer
+     * @return valeur à la position index
+     */
+
     public T get(int index) {
         T value = null;
         for (Pair<Integer, T> p : this.array) {
@@ -36,31 +55,65 @@ public class RangeArray<T> implements Iterable{
 
     }
 
+    /**
+     *
+     * @return indice minimum
+     */
+
     public int getIndexMin() {
         return this.indexMin;
     }
+
+    /**
+     *
+     * @param indexMin indice minimum à set
+     */
 
     public void setIndexMin(int indexMin) {
         this.indexMin = indexMin;
     }
 
+    /**
+     *
+     * @return indice maximum
+     */
+
     public int getIndexMax() {
         return indexMax;
     }
+
+    /**
+     *
+     * @param indexMax indice maximum à set
+     */
 
     public void setIndexMax(int indexMax) {
         this.indexMax = indexMax;
     }
 
+    /**
+     * Fonction set générique
+     *
+     * @param key clé de la valeur
+     * @param value valeur
+     */
     public void set(Integer key, T value) {
         this.array.add(new Pair<>(key, value));
 
     }
 
+    /**
+     *
+     * @return taille du tableau
+     */
     public int rangeSize() {
         return this.array.size();
     }
 
+    /**
+     * Fonction qui rend la classe RangeArray itérable
+     * @return l'iterator à utiliser
+     */
     @Override
     public Iterator iterator() {
         return null;
