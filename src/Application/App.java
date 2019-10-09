4e0c66c4dc2f8f73ef772d7c70abb35b99c36be5
theo-poujol <theo.poujol@etu.univ-amu.fr>
2019-10-09 21:24:47 +0200

    Test main with handle exceptions

--- src/Application/App.java
+++ src/Application/App.java
@@ -1,5 +1,10 @@
 public class App {
-    public static void main(String args[]) {
-        System.out.println("Hello world");
+    public static void main(String args[]) throws IndiceInferieurException {
+
+        RangeArray<String> habitations = new RangeArray<>(3, 5);
+        System.out.println(habitations.rangeSize());
+        habitations.set(3, "Maison");
+        habitations.set(4, "Immeuble");
+        habitations.set(5, "Hutte");
     }
 }
